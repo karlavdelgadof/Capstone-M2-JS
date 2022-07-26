66936dc7a4029e79a1db0821917196db594c35ad
karlavdelgadof <karlavdelgadof@gmail.com>
2022-07-26 13:20:12 -0400

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -24,7 +24,7 @@ module.exports = {
       title: 'Development',
       template: './src/index.html',
     }),
-    new FaviconsWebpackPlugin('./src/favicon-32x32.png')
+    new FaviconsWebpackPlugin('./src/favicon-32x32.png'),
   ],
   output: {
     filename: '[name].bundle.js',
