ed5b623d0431c44fbeb673198da0c78afbe9ef05
wayungi <fwayungi@gmail.com>
2022-07-28 12:14:50 +0300

    Add show comment-popup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import artistsAPI from './modules/getArtistAPI';
 import createCard from './modules/homeUI';
 import './style.css';
 
+const artistList = document.getElementById('artist-list');
+const commentPopup = document.getElementById('comment_popup');
+console.log(commentPopup);
+
+artistList.addEventListener('click', (e) => {
+  if (e.target.className === 'comment') {
+    // show popup window
+    commentPopup.classList.remove('hide');
+    commentPopup.classList.add('show');
+  }
+});
+
 document.addEventListener('DOMContentLoaded', async () => {
   const artists = await artistsAPI.getArtist();
-  console.log(artists)
-  artists.forEach(createCard)
+  console.log(artists);
+  artists.forEach(createCard);
 });
