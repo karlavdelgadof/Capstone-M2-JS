7d557d33b060d3472adbd827e4284d09137659e6
karlavdelgadof <karlavdelgadof@gmail.com>
2022-07-29 18:50:25 -0400

    Add test for items counter

---
+++ src/modules/test/itemsCounter.test.js
@@ -0,0 +1,11 @@
+/** * @jest-environment jsdom */ 
+
+test("Array [1,2,3,4,5,6] should return 6", () => {
+  const mockAPI = () => Promise.resolve({ items: [1, 2, 3, 4, 5, 6] });
+    mockAPI().then((result) => {
+      const artistArr = result.items;
+      const artistCounter = artistArr.length;
+      expect(artistCounter).toBe(6);
+    })
+});
+
