cba0405162d54accee69c1e5f8b1d0800ccbff92
karlavdelgadof <karlavdelgadof@gmail.com>
2022-07-29 20:25:44 -0400

    Fix linter errors

--- src/modules/test/itemsCounter.test.js
+++ src/modules/test/itemsCounter.test.js
@@ -1,11 +1,10 @@
-/** * @jest-environment jsdom */ 
+/** * @jest-environment jsdom */
 
-test("Array [1,2,3,4,5,6] should return 6", () => {
+test('Array [1,2,3,4,5,6] should return 6', () => {
   const mockAPI = () => Promise.resolve({ items: [1, 2, 3, 4, 5, 6] });
-    mockAPI().then((result) => {
-      const artistArr = result.items;
-      const artistCounter = artistArr.length;
-      expect(artistCounter).toBe(6);
-    })
+  mockAPI().then((result) => {
+    const artistArr = result.items;
+    const artistCounter = artistArr.length;
+    expect(artistCounter).toBe(6);
+  });
 });
-
